bc6e19d5de610eb7e47c0025dcddb2be9a0cea78
tanbirk <tanvir.kashyap879@gmail.com>
2021-11-26 18:58:48 +0530

    almost done

--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -1,11 +1,11 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
 const styles = StyleSheet.create ({
 
     
   carContainer: {
     width: '100%',
-    height:'100%',
+    height: Dimensions.get('window').height
 
 },
 titles: {
